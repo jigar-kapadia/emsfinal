ea1a865c404e4a07d8bb179250fac3caadf1e519
Jigar kapadia <jigar.kapadia@learningmatemumbai.local>
2018-06-25 18:12:45 +0530

    minute http changes hahaha

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.listen(process.env.PORT || 3000);
 if (process.env.NODE_ENV == 'production') {
     app.use(function (req, res, next) {
         res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
-        if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
+        if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "https") {
             return res.redirect(301, 'http://' + req.host + req.url);
         } else {
             return next();
