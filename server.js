7e38315dbeec08a7ebdcbf1be222aaf870e569c5
Jigar kapadia <jigar.kapadia@learningmatemumbai.local>
2018-06-25 17:42:05 +0530

    http changes in server js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-
+const http = require('http');
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
