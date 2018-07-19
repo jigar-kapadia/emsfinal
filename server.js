c031254dc3fa5458fe0cb6740322d94dc6fcaa9e
Jigar kapadia <jigar.kapadia@learningmatemumbai.local>
2018-07-19 21:23:09 +0530

    added validation and new createhtml

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const http = require('http');
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 3000);
 
-if (process.env.NODE_ENV == 'production') {
-    app.use(function (req, res, next) {
-        res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
-        if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "https") {
-            return res.redirect(301, 'http://' + req.host + req.url);
-        } else {
-            return next();
-            }
-    });
-}
+// if (process.env.NODE_ENV == 'production') {
+//     app.use(function (req, res, next) {
+//         res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
+//         if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "https") {
+//             return res.redirect(301, 'http://' + req.host + req.url);
+//         } else {
+//             return next();
+//             }
+//     });
+// }
